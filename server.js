c55e254d8b505fa45c48d0ed3ed56c608406ffe5
Evans <flomage2@outlook.com>
2025-07-21 20:38:58 -0400

    potluck backend setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Potluck backend testing');
+});
+
+// Connect to MongoDB and start server
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(err => console.log(err));
