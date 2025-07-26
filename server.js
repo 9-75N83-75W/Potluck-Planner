4df6e04c2a8eaf8e18fe82e250dd26c1611af36b
Ariel Zolton <163615121+ArielZolton@users.noreply.github.com>
2025-07-25 20:39:32 -0500

    added default localhost server and changed port

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Test route
 app.use('/api',allroutes)
 
 // Connect to MongoDB and start server
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/potluck", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
